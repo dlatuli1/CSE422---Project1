e5bf1857f380892ee66b7594f81cff32ce017abe
dlatuli1 <dlatuli1@yahoo.com>
2015-09-29 17:59:56 -0500

    Update main.cpp
    improved history command.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -19,18 +18,19 @@ int main(int argc, char* argv[])
 	vector<string> argvector;
 	for (;;)
 	{
-		
+
 		parse_input_line(argvector);
 
 
-		if (argvector[0] == "history")
+		if (built_in_command(argvector[0]))
 		{
-			history(argvector[1]);
+			//do command func.
+			history(argvector[1]); //history might not have an argument - address this
 		}
 
 
 		argvector.clear();
-		
+
 	} // main for loop /
 }
 
@@ -43,25 +43,17 @@ int parse_input_line(vector<string>& arg_vector)
 	cout << "sish >>";
 	getline(cin, input);
 
-	if (commandHistory.size() >= 100) //limit history deque size to 100
-	{
-		commandHistory.erase(commandHistory.begin());
-	}
+	if (commandHistory.size() >= 100) commandHistory.erase(commandHistory.begin()); //limit history deque size to 100
 	commandHistory.push_back(input);
-	
-	while(delimiter!=string::npos)
+	while (delimiter != string::npos)
 	{
 		delimiter = input.find(' ');
 		if (delimiter != string::npos)
 		{
 			arg_vector.push_back(input.substr(0, delimiter));
-			input.erase(0, delimiter+1);
+			input.erase(0, delimiter + 1);
 		}
-		else
-		{
-			arg_vector.push_back(input.substr(0, string::npos));
-
-		}
+		else arg_vector.push_back(input.substr(0, string::npos));
 	}
 	return 0;
 }
@@ -73,38 +65,33 @@ int parse_input_line(vector<string>& arg_vector)
 // should be pretty close, TODO - no error handling other than isdigit
 int history(string historyStr)
 {
+	unsigned int count = 0, history_asked = 0;
 
-	int lineNum = 0, historyNum = 0;
-	
-	if (isdigit(historyStr[0])) historyNum = atoi(historyStr.c_str());
-	else return 1;
-	if (historyNum < commandHistory.size())
+	if (isdigit(historyStr[0])) history_asked = atoi(historyStr.c_str());  //converting string parameter to int
+	else history_asked = commandHistory.size(); //if user doesnt enter a numbered parameter, dump it all		**TODO**: crashes due to function call
+
+	if (history_asked > commandHistory.size()) history_asked = commandHistory.size(); //trimming max history asked to current size
+
+	for (unsigned int i = (commandHistory.size() - history_asked - 1); i < commandHistory.size() - 1; i++)  // '-1's are so current history command is not printed to console;
 	{
-		for (unsigned int i = (commandHistory.size() - historyNum - 1); i < commandHistory.size() - 1; i++)  // '-1's are so current history command is not printed to console;
-		{
-			++lineNum;
-			cout << '\t' << lineNum << "  " << commandHistory[i] << '\n';
-		}
+		cout << '\t' << ++count << "  " << commandHistory[i] << '\n';
 	}
-	else
-	{
-		//error stuff?
-	}
+
 	return 0;
 }
 
 
 // verify if argument is a built-in command
 bool built_in_command(string arg) {
-    
-    // array of built-in commands
-    static const string built_in_commands_array[] = {"exit", "clr", "echo", "show", "environ", "export", "unexport", "set", "unset", "dir", "chdir", "history", "repeat", "kill", "fork", "pause", "wait", "help"};
-    // make a vector of built-in commands using the above array
-    vector<string> built_in_commands (built_in_commands_array, built_in_commands_array + sizeof(built_in_commands_array) / sizeof(built_in_commands_array[0]) );
-    if (find(built_in_commands.begin(), built_in_commands.end(), arg) != built_in_commands.end())
-        return true;
-    else
-        return false;
+
+	// array of built-in commands
+	static const string built_in_commands_array[] = { "exit", "clr", "echo", "show", "environ", "export", "unexport", "set", "unset", "dir", "chdir", "history", "repeat", "kill", "fork", "pause", "wait", "help" };
+	// make a vector of built-in commands using the above array
+	vector<string> built_in_commands(built_in_commands_array, built_in_commands_array + sizeof(built_in_commands_array) / sizeof(built_in_commands_array[0]));
+	if (find(built_in_commands.begin(), built_in_commands.end(), arg) != built_in_commands.end())
+		return true;
+	else
+		return false;
 }
 
 
@@ -112,61 +99,61 @@ bool built_in_command(string arg) {
 //STUFF FROM SLIDES
 
 /*		if built_in_command(arg_vector[0])
-		{
-			do_it(arg_vector);
-			continue;
-		} //built_in command
-		pathname = find_path(arg_vector[0]);
-		if ((pid = fork()) == 0)
-		{
-			// code executed by child
-			if (!ispiped)
-			{
-				if (stdout_redirect)
-				{ // open target file and create it if needed
-					fd = open(outfile, O_WRONLY | O_CREAT, 0644);
-					close(1); // close stdout/
-					dup(fd); // dup into stdout
-					close(fd); // good practice
-				}
-				if (stdin_redirect)
-				{
-					fd = open(infile, O_RDONLY);
-					close(0); // close stdin/
-					dup(fd); // dup into stdin
-					close(fd); // good practice
-				}
-				execve(pathname, arg_vector, envp);
-				exit(1); // in case execve fails 
-			}
-			else exec_pipe();
-		} //child process
-		if (interactive())
-		{
-			while (wait(0) != pid);
-		}*/
-	
+{
+do_it(arg_vector);
+continue;
+} //built_in command
+pathname = find_path(arg_vector[0]);
+if ((pid = fork()) == 0)
+{
+// code executed by child
+if (!ispiped)
+{
+if (stdout_redirect)
+{ // open target file and create it if needed
+fd = open(outfile, O_WRONLY | O_CREAT, 0644);
+close(1); // close stdout/
+dup(fd); // dup into stdout
+close(fd); // good practice
+}
+if (stdin_redirect)
+{
+fd = open(infile, O_RDONLY);
+close(0); // close stdin/
+dup(fd); // dup into stdin
+close(fd); // good practice
+}
+execve(pathname, arg_vector, envp);
+exit(1); // in case execve fails
+}
+else exec_pipe();
+} //child process
+if (interactive())
+{
+while (wait(0) != pid);
+}*/
+
 
 
 /*
 exec_pipe() {
-	int pd[2];
-	pipe(pd); //creates the pipe
-	...
-		if ((pid = fork()) == 0) {
-			// child does producer/
-			close(1); // close stdout
-			dup(pd[1]); // dup into stdout
-			close(pd[0]); close(pd[1]); //
-			goody
-				execve(leftsidecommand, p_argv,
-					p_envp); exit(1); // should execve fail
-		} //* end of child
-		  // parent does consumer
-	close(0); // close stdin
-	dup(pd[0]; // dup into stdin
-	close(pd[0]); close(pd[1]); //
-	goody
-		execve(rightsidecommand, c_argv,
-			c_envp); exit(2); // if execve fails
+int pd[2];
+pipe(pd); //creates the pipe
+...
+if ((pid = fork()) == 0) {
+// child does producer/
+close(1); // close stdout
+dup(pd[1]); // dup into stdout
+close(pd[0]); close(pd[1]); //
+goody
+execve(leftsidecommand, p_argv,
+p_envp); exit(1); // should execve fail
+} //* end of child
+// parent does consumer
+close(0); // close stdin
+dup(pd[0]; // dup into stdin
+close(pd[0]); close(pd[1]); //
+goody
+execve(rightsidecommand, c_argv,
+c_envp); exit(2); // if execve fails
 }*/
