7e6460021d82ab6326020945f91ce1bd7aca108f
dlatuli1 <dlatuli1@yahoo.com>
2015-09-28 22:40:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,52 @@
 
+
 #include <iostream>
 #include <string>
 #include <vector>
+#include <deque>
 
 using namespace std;
 
+deque<string> commandHistory;
 int parse_input_line(vector<string>& argvector);
-
-vector<string> commandHistory;
+int history(string historyStr);
 
 int main(int argc, char* argv[])
 {
-	cout << "sish >>";
+	//cout << "sish >>";
 	vector<string> argvector;
 	for (;;)
 	{
+		
 		parse_input_line(argvector);
-		//
-		
-		// stuff
-		
-		//
+
+
+		if (argvector[0] == "history")
+		{
+			history(argvector[1]);
+		}
+
+
 		argvector.clear();
+		
 	} // main for loop /
 }
+
 
 int parse_input_line(vector<string>& arg_vector)
 {
 	string input = "";
 	size_t delimiter = 0;
+
+	cout << "sish >>";
 	getline(cin, input);
+
+	if (commandHistory.size() >= 100) //limit history deque size to 100
+	{
+		commandHistory.erase(commandHistory.begin());
+	}
 	commandHistory.push_back(input);
+	
 	while(delimiter!=string::npos)
 	{
 		delimiter = input.find(' ');
@@ -45,6 +61,102 @@ int parse_input_line(vector<string>& arg_vector)
 
 		}
 	}
-	cout << "sish >>";
 	return 0;
 }
+
+
+
+
+// implemeting history command
+// should be pretty close, TODO - no error handling other than isdigit
+int history(string historyStr)
+{
+
+	int lineNum = 0, historyNum = 0;
+	
+	if (isdigit(historyStr[0])) historyNum = atoi(historyStr.c_str());
+	else return 1;
+	if (historyNum < commandHistory.size())
+	{
+		for (unsigned int i = (commandHistory.size() - historyNum - 1); i < commandHistory.size() - 1; i++)  // '-1's are so current history command is not printed to console;
+		{
+			++lineNum;
+			cout << '\t' << lineNum << "  " << commandHistory[i] << '\n';
+		}
+	}
+	else
+	{
+		//error stuff?
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+//STUFF FROM SLIDES
+
+/*		if built_in_command(arg_vector[0])
+		{
+			do_it(arg_vector);
+			continue;
+		} //built_in command
+		pathname = find_path(arg_vector[0]);
+		if ((pid = fork()) == 0)
+		{
+			// code executed by child
+			if (!ispiped)
+			{
+				if (stdout_redirect)
+				{ // open target file and create it if needed
+					fd = open(outfile, O_WRONLY | O_CREAT, 0644);
+					close(1); // close stdout/
+					dup(fd); // dup into stdout
+					close(fd); // good practice
+				}
+				if (stdin_redirect)
+				{
+					fd = open(infile, O_RDONLY);
+					close(0); // close stdin/
+					dup(fd); // dup into stdin
+					close(fd); // good practice
+				}
+				execve(pathname, arg_vector, envp);
+				exit(1); // in case execve fails 
+			}
+			else exec_pipe();
+		} //child process
+		if (interactive())
+		{
+			while (wait(0) != pid);
+		}*/
+	
+
+
+/*
+exec_pipe() {
+	int pd[2];
+	pipe(pd); //creates the pipe
+	...
+		if ((pid = fork()) == 0) {
+			// child does producer/
+			close(1); // close stdout
+			dup(pd[1]); // dup into stdout
+			close(pd[0]); close(pd[1]); //
+			goody
+				execve(leftsidecommand, p_argv,
+					p_envp); exit(1); // should execve fail
+		} //* end of child
+		  // parent does consumer
+	close(0); // close stdin
+	dup(pd[0]; // dup into stdin
+	close(pd[0]); close(pd[1]); //
+	goody
+		execve(rightsidecommand, c_argv,
+			c_envp); exit(2); // if execve fails
+}*/
