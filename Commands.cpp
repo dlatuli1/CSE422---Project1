785cf584e2df94b283677aa5531801a71681d4f9
tanliuyuan <tanliuyuan@aliyun.com>
2015-09-30 20:12:18 -0500

    Implemented the first 5 commands

--- Commands.cpp
+++ Commands.cpp
@@ -2,21 +2,28 @@
 #include "main.h"
 #include <iostream>
 #include <map>
+#include <string>
+#include <stdlib.h>
 
-map<string string> localVariable;
+map<string, string*> localVariable;
 
 int show(vector<string> argVector)
 {
    //Execute show command
+    for (int i = 1; i < argVector.size(); i++) {
+        cout << argVector[i] << endl;
+    }
    return 0;
 }
 
 int set(vector<string> argVector)
 {
    //Execute set command
-    if (argVector.size == 3) {
+    if (argVector.size() == 3) {
         localVariable[argVector[1]] = new string();
-        localVariable[argVector[1]] = argVector[2];
+        localVariable[argVector[1]] = &argVector[2];
+    } else {
+        cout << "Usage: set W1 W2" << endl;
     }
    return 0;
 }
@@ -24,18 +31,46 @@ int set(vector<string> argVector)
 int unset(vector<string> argVector)
 {
    //Execute unset command
+    if (argVector.size() == 2) {
+        if (localVariable[argVector[1]]) {
+            delete localVariable[argVector[1]];
+        }
+    } else {
+        cout << "Usage: unset W" << endl;
+    }
    return 0;
 }
 
-int exportVariables(vector<string>argVector)
+int exportVariable(vector<string>argVector)
 {
    //Execute export command
+    if (argVector.size() == 3) {
+        // since setenv() takes char* type, we have to first convert the arguments into char*
+        char *cStringArg1 = new char[argVector[1].length() + 1];
+        char *cStringArg2 = new char[argVector[2].length() + 1];
+        strcpy(cStringArg1, argVector[1].c_str());
+        strcpy(cStringArg2, argVector[2].c_str());
+        setenv(cStringArg1, cStringArg2, true);
+        delete cStringArg1;
+        delete cStringArg2;
+    } else {
+        cout << "Usage: set W1 W2" << endl;
+    }
    return 0;
 }
 
 int unexport(vector<string>argVector)
 {
    //Execute unexport command
+    if (argVector.size() == 2) {
+        // since unsetenv() takes char* type, we have to first convert the arguments into char*
+        char *cStringArg = new char[argVector[1].length() + 1];
+        strcpy(cStringArg, argVector[1].c_str());
+        unsetenv(cStringArg);
+        delete cStringArg;
+    } else {
+        cout << "Usage: unexport W" << endl;
+    }
    return 0;
 }
 
