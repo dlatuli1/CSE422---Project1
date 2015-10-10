1b9aa83e495843b25b1f43919033f8844a5096c6
cglaser <cglaser@wustl.edu>
2015-10-09 22:01:39 -0500

    add start of command line flags

--- main.cpp
+++ main.cpp
@@ -4,6 +4,28 @@ int main(int argc, char* argv[])
 {
    SimpleShell* SISH = new SimpleShell();
    
+   for (int i = 1; i < argc-1; i++)
+   {
+      char c = argv[i][1];
+
+      switch (c)
+      {
+          case 'f':
+             //do file input stuff
+             i++;
+             break;
+          case 'x':
+             //do variable substitution stuff
+             break;
+          case 'd':
+             //do debug level stuff
+             i++;
+             break;
+          default:
+             cout << "Invalid command line flag -" << c << endl;
+       }
+    }
+
    SISH->ShellLoop();
 
    return 0;
