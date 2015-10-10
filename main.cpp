6d628f460883ff2dcb49c128542ac44ffa419a51
cglaser <cglaser@wustl.edu>
2015-10-10 12:05:27 -0500

    code cleanup

--- main.cpp
+++ main.cpp
@@ -3,35 +3,35 @@
 int main(int argc, char* argv[])
 {
    SimpleShell* SISH = new SimpleShell();
-   
-   for (int i = 1; i < argc-1; i++)
+
+   for (int i = 1; i < argc - 1; i++)
    {
       char c = argv[i][1];
 
       switch (c)
       {
-          case 'f':
-	  {
-             i++;
-             string inputFile = argv[i];
-	     SISH->SetFileInputMode(true, inputFile);
-             break;
-	  }
-          case 'x':
-	  {
-             //do variable substitution stuff
-             break;
-	  }
-          case 'd':
-	  {
-             //do debug level stuff
-             i++;
-             break;
-	  }
-          default:
-             cout << "Invalid command line flag -" << c << endl;
-       }
-    }
+      case 'f':
+      {
+         i++;
+         string inputFile = argv[i];
+         SISH->SetFileInputMode(true, inputFile);
+         break;
+      }
+      case 'x':
+      {
+         //do variable substitution stuff
+         break;
+      }
+      case 'd':
+      {
+         //do debug level stuff
+         i++;
+         break;
+      }
+      default:
+         cout << "Invalid command line flag -" << c << endl;
+      }
+   }
 
    SISH->ShellLoop();
 
