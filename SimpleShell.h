7f87a33c0341c14adae1b072e3415c3a4a2c27a7
cglaser <cglaser@wustl.edu>
2015-10-10 12:04:21 -0500

    code cleanup

--- SimpleShell.h
+++ SimpleShell.h
@@ -20,23 +20,23 @@ public:
    // ************************
 
    // Constructor
-	SimpleShell();
+   SimpleShell();
 
    // Deconstructor
-	~SimpleShell();
+   ~SimpleShell();
 
    // Main Loop
-	void ShellLoop();
+   void ShellLoop();
 
-	void SetFileInputMode(bool fim, string input);
-	void SetDebugLevel(int dl);
-	void SetVariableSubstitution(bool vs);
+   void SetFileInputMode(bool fim, string input);
+   void SetDebugLevel(int dl);
+   void SetVariableSubstitution(bool vs);
 
-	void InitSigHandler();        //Inits the sigaction structs
-	void InitEnvironment();
-	static void HandleSIGNAL(int sig);    //Handles the SIGINT signal
-	void VariableSub();
-	bool CheckPiped();
+   void InitSigHandler();        //Inits the sigaction structs
+   void InitEnvironment();
+   static void HandleSIGNAL(int sig);    //Handles the SIGINT signal
+   void VariableSub();
+   bool CheckPiped();
 
 private:
    // *************************
@@ -44,50 +44,25 @@ private:
    // *************************
 
    // Parse Input Line
-	int ParseInputLine();
+   int ParseInputLine();
 
    // Execute Command
-	Command::ShellStates ExecuteCommand();
+   Command::ShellStates ExecuteCommand();
 
    // **********************
    // *** Private fields ***
    // **********************
-	vector<string> argVector;
-	vector<string> pipedVector;// holds commands - delimited by pipes. keeps arguments space delimited in same slot as command
-	bool isPiped;
-    static vector<pid_t> ForegroudProcesses;//holds all running
-    static vector<pid_t> BackgroudProcesses;
-	History* ShellCommandHistory;
-	Command* ShellCommand;
-	
-	bool fileInputMode;
-	ifstream inputFile;
+   vector<string> argVector;
+   vector<string> pipedVector;// holds commands - delimited by pipes. keeps arguments space delimited in same slot as command
+   bool isPiped;
+   static vector<pid_t> ForegroudProcesses;//holds all running
+   static vector<pid_t> BackgroudProcesses;
+   History* ShellCommandHistory;
+   Command* ShellCommand;
 
-        int debugLevel;
-	bool variableSubstitution;
+   bool fileInputMode;
+   ifstream inputFile;
+
+   int debugLevel;
+   bool variableSubstitution;
 };
-
-class ShellControl
-{
-public:
-
-    ShellControl();// Constructor
-    ~ShellControl();// Deconstructor
-
-    void InitSigHandler();        //Inits the sigaction structs
-    static void HandleSIGNAL(int sig);    //Handles the SIGINT signal
-//    static void HandleSIGQUIT(int sig);   //Handles the SIGQUIT signal
-//    static void HandleSIGCONT(int sig);   //Handles the SIGCONT signal
-//    static void HandleSIGSTP(int sig);    //Handles the SIGSTP signal
-//    static void HandleSIGIGNORE(int sig); //Ignore the rest
-    bool CheckPiped();
-
-
-private:
-    static vector<pid_t> ForegroudProcesses;//holds all running
-    static vector<pid_t> BackgroudProcesses;
-    bool pipedCommand;
-//struct sigaction action;
-};
-
-
