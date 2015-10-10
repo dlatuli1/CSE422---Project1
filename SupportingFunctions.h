86b0bdf281dc16436840626de9be98db881404ee
cglaser <cglaser@wustl.edu>
2015-10-10 12:30:02 -0500

    Update SupportingFunctions.h

--- SupportingFunctions.h
+++ SupportingFunctions.h
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <string>
+#include <cstring>
+#include <sstream>
 
 using namespace std;
 
@@ -9,4 +11,11 @@ char *convert(const string & s)
     char *pc = new char[s.size()+1];
     strcpy(pc, s.c_str());
     return pc;
+}
+
+string to_string(const int & i)
+{
+   ostringstream convert;
+   convert << i;
+   return convert.str();
 }
